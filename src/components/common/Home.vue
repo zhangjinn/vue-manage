2b234ceac226c9a5a71209f91a1b1384ed49e8bc
zhangjinn <zhangjinn92@163.com>
2018-11-09 17:36:12 +0800

    change

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -2,11 +2,11 @@
   <div class="mod-home">
     <h3>项目介绍</h3>
     <ul>
-      <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li>
+      <li>vue-manage基于vue、element-ui构建开发，实现后台管理前端功能，提供一套更优的前端解决方案</li>
       <li>前后端分离，通过token进行数据交互，可独立部署</li>
       <li>主题定制，通过scss变量统一一站式定制</li>
       <li>动态菜单，通过菜单管理统一管理访问路由</li>
-      <li>演示地址：<a href="http://fast.demo.renren.io" target="_blank">fast.demo.renren.io</a> (账号密码：admin/admin)</li>
+      <li>演示地址：<a href="javascript:void(0)" target="_blank">vue-manage</a> (账号密码：admin/admin)</li>
     </ul>
   </div>
 </template>
